100040f804d58b1330047da4d8f3f15872b18be3
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-12-06 03:25:29 +0100

    recherche ville bon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,8 +16,14 @@
 <body style="overflow-x:hidden;">
     <div  class="ciel weather-box ensoleille" id="weatherDiv">
     <!-- meteo -->
-        <p>Météo ensoleillée par défaut...</p>
+        <div id="searchContainer" class="search-container">
+            <input type="text" id="cityInput" placeholder="Entrez une ville..." class="hidden">
+            <img id="loupe" src="assets/images/loupe.png" alt="recherche-ville">
+        </div>
+        <p>Chargement de la météo...</p>
         <img id="weatherImage" src="assets/images/ensoleille.gif" alt="Ensoleillé">
+
+        <script src="assets/script/meteo.js"></script>
     
     
         <div class="container">
@@ -69,6 +75,5 @@
 <?php
     require_once __DIR__."/./page/defis.php";
 ?>
-<script src="assets/script/meteo.js"></script>
 </body>
 </html>
