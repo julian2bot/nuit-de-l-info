3c04dab08281b893c72b255507b8b2d85fbd12a7
julian-marques <juliandronefpv@gmail.com>
2024-12-06 05:57:05 +0100

    suppr reset point

--- page/defis.php
+++ page/defis.php
@@ -1,7 +1,9 @@
 <?php
     // require_once __DIR__."/./../utils/start.php";
     // print_r($_SESSION);
-    echo "<a id='reset' href='./page/resetPoint.php'>Reset Point</a>";
+    // debug lol
+    // echo "<a id='reset' href='./page/resetPoint.php'>Reset Point</a>";
+
 
     echo "<p id='leScore'> Score : ".$_SESSION["user"]["score"]."</p>";
     
